af0293c01c413575b386902057c608631ceb54cb
Kaua Hl <profissional.kaua@gmail.com>
2022-06-02 17:00:00 -0300

    feat: added error component

---
+++ src/components/Error/Error.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Error.module.css";
+
+const Error = () => {
+  return <span className={styles.error}>Por favor digite seu E-mail</span>;
+};
+
+export default Error;
