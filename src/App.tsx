1c2d61440aa7f3a89b84fb2a0838c473af4dc3d7
Kaua Hl <profissional.kaua@gmail.com>
2022-06-02 20:56:26 -0300

    refactor: adding settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import "./assets/css/index.css";
 import Header from "./components/Header/Header";
-import Banner from "./components/Home/Banner/Banner";
+import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <>
       <Header />
-      <Banner />
+      <Home />
     </>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Apartments from "../../components/Home/Apartments/Apartments";
 import Banner from "../../components/Home/Banner/Banner";
+import Features from "../../components/Home/Features/Features";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Features />
+      <Apartments />
     </div>
   );
 };
