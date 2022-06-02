e687c6ca20653a3eb59a052eab1d9dc746027574
Kaua Hl <profissional.kaua@gmail.com>
2022-06-02 17:03:53 -0300

    refactor: basic settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import "./App.css";
+import "./assets/css/index.css";
+import Header from "./components/Header/Header";
+import Banner from "./components/Home/Banner/Banner";
 import Login from "./pages/Login/Login";
 
 function App() {
-  return <Login />;
+  return (
+    <>
+      <Header />
+      <Banner />
+    </>
+  );
 }
 
 export default App;
