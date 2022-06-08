594929316caab110b2031887f8bc0463e0103e5f
Kaua Hl <profissional.kaua@gmail.com>
2022-06-07 21:59:17 -0300

    feat: firebase register

---
+++ src/services/firebase/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIGhjUHq_3N_JmdpTdclHAKjsDiqIiBRE",
+  authDomain: "project-1-6dd44.firebaseapp.com",
+  projectId: "project-1-6dd44",
+  storageBucket: "project-1-6dd44.appspot.com",
+  messagingSenderId: "932482325088",
+  appId: "1:932482325088:web:cb6ad7470ba77889d7c105",
+};
+
+initializeApp(firebaseConfig);
